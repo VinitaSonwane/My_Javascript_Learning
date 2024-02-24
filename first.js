fffbd225ca3f129e67d50f5cbf9e6d23d868f950
VinitaSonwane <VinitaSonwane>
2024-02-24 10:23:42 +0530

    commit

--- first.js
+++ first.js
@@ -1,10 +1,36 @@
-fullName = " Vinita Sonwane";
+/*fullName = " Vinita Sonwane";
 age = 24;
 price = 99.9;
 x = null;
 y = undefined;
 isFollow = true;
 
-fullName = 25;
+FULLNAME = "chaitu"
+isFollow = "false";
+console.log("Apna College");
+console.log(FULLNAME);
 
-console.log(fullName);
+console.log(isFollow);
+
+let name = "tony stark";
+let totalPrice = 1000;
+const PI = 3.14;
+console.log(PI);
+console.log(name);
+console.log(totalPrice);
+
+
+//blocks
+{
+    let a = 5;
+    console.log(a);
+} {
+    let a = 10;
+    console.log(a);
+} */
+
+//data types 
+let age = 24;
+let price = 100.5;
+
+let fullName = "Vinita";
---
+++ script.js
@@ -0,0 +1,58 @@
+//Arithmetic operator
+//let a = 5;
+//let b = 3;
+
+
+//console.log("a = ", a, " & b = ", b);
+//console.log("a - b =", a - b);
+//console.log("a + b = ", a + b);
+//console.log("a * b = ", a * b);
+//console.log("a / b = ", a / b);
+//console.log("a % b = ", a % b);
+//console.log("a ** b = ", a ** b);
+//console.log("-- a = ", --a);
+//console.log("a-- = ", a--);
+
+//a += 4;
+//console.log("a == ", a);
+
+
+//let a = 5;
+//let b = "5";
+//console.log("a === b", a === b);
+//console.log(" a !== b ", a !== b);
+
+/*let a = 5;
+let b = 4;
+
+let cond1 = a > b; //true
+let cond2 = a != b; //true
+//console.log("cond1 && cond2 : ", cond1 && cond2);
+console.log("cond1 && cond2 = ", a === b || a > b);
+
+let age = 25;
+if (age > 18) {
+    console.log("you can vote ");
+} * /
+
+/*let num = 10;
+if (num % 2 === 0) {
+    console.log(num, "num is even");
+} else {
+    console.log("num is odd ");
+}
+
+if (age < 18) {
+    console.log("junior");
+} else if (age < 30) {
+    console.log("senior");
+} else {
+    console.log("not have rights ")
+} */
+
+
+
+//let name = prompt("hello");
+//console.log(name);
+let number = prompt("Enter a number ");
+console.log(number);
